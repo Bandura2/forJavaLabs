faf2a0637b8a945f9e9071e811240c9cd8a8189f
Bandura2 <119165162+Bandura2@users.noreply.github.com>
2023-03-27 04:10:38 +0300

    Changed files to second lab

--- src/ua/lviv/iot/algo/part1/lab2/InsectManager.java
+++ src/ua/lviv/iot/algo/part1/lab2/InsectManager.java
@@ -36,34 +36,34 @@ public class InsectManager {
     }
     public static void main(String[] args)
     {
-        Insect mosquito1 = new Mosquito("Vasya", 4,true, false, false);
-        Insect mosquito2 = new Mosquito("Artur", 6, true, true, true);
-        Insect hornet1 = new Hornet("Locky", 2, true, false, true);
-        Insect hornet2 = new Hornet("Herlok", 4, true, true, false);
-        Insect butterfly1 = new Butterfly("Filuna", 8, true, false, false);
-        Insect butterfly2 = new Butterfly("Dunar", 6, true, false, true);
-        Insect mantis1 = new Mantis("Olena", 8, false, true, false);
-        Insect mantis2 = new Mantis("Zoro", 2, false, true, true);
+        Insect MosquitoFirst = new Mosquito("Vasya", 4,true, false, false);
+        Insect MosquitoSecond = new Mosquito("Artur", 6, true, true, true);
+        Insect HornetFirst = new Hornet("Locky", 2, true, false, true);
+        Insect HornetSecond = new Hornet("Herlok", 4, true, true, false);
+        Insect ButterflyFirst = new Butterfly("Filuna", 8, true, false, false);
+        Insect ButterflySecond = new Butterfly("Dunar", 6, true, false, true);
+        Insect MantisFirst= new Mantis("Olena", 8, false, true, false);
+        Insect MantisSecond = new Mantis("Zoro", 2, false, true, true);
 
-        InsectManager manager = new InsectManager();
-        manager.addInsect(mosquito1);
-        manager.addInsect(mosquito2);
-        manager.addInsect(hornet1);
-        manager.addInsect(hornet2);
-        manager.addInsect(butterfly1);
-        manager.addInsect(butterfly2);
-        manager.addInsect(mantis1);
-        manager.addInsect(mantis2);
+        InsectManager Manager = new InsectManager();
+        Manager.addInsect(MosquitoFirst);
+        Manager.addInsect(MosquitoSecond);
+        Manager.addInsect(HornetFirst);
+        Manager.addInsect(HornetSecond);
+        Manager.addInsect(ButterflyFirst);
+        Manager.addInsect(ButterflySecond);
+        Manager.addInsect(MantisFirst);
+        Manager.addInsect(MantisSecond);
 
         System.out.println();
-        manager.testAbstractMethods();
+        Manager.testAbstractMethods();
 
         System.out.println("Printing all iinsects that are dangereous:");
-        for (Insect ins : manager.findAllDangerous()) {
+        for (Insect ins : Manager.findAllDangerous()) {
             System.out.println(ins);
         }
         System.out.println("\nPrinting all iinsects that have more than 4 legs : ");
-        for (Insect ins : manager.findAllWithNumberOfLegsGreaterThan(4)) {
+        for (Insect ins : Manager.findAllWithNumberOfLegsGreaterThan(4)) {
             System.out.println(ins);
         }
     }
